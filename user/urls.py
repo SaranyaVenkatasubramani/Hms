03533a5ca45fc4c95fb95c6171cae63095cb8bcd
SaranyaAV <vensaranya2007@gmail.com>
2025-07-18 15:23:44 +0530

    final

--- user/urls.py
+++ user/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import homepage
 from .views import login
 urlpatterns = [
-    path('', homepage, name='user-home'),
-    path('login', login,name='user-login'),
+    path('',views.homepage, name='user-home'),
+   
 ]
 
 
--- user/views.py
+++ user/views.py
@@ -3,7 +3,4 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'user/index.html')
-def login(request):
-    return render(request,'user/login.html')
 
-# Create your views here.
