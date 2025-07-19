cf3d2c29b679e7c56241caaedcfa406ef13a068f
SaranyaAV <vensaranya2007@gmail.com>
2025-07-19 15:14:33 +0530

    victory

--- patients/views.py
+++ patients/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 def patient_dashboard(request):
     return render(request,'patient_dashboard.html')
 def doctor(request):
     return render(request,'doctor.html')
 def book(request):
     return render(request,'book.html')
-=======
 
-def patient_login(request):
-    return render(request, 'patients/patdashboard.html')
->>>>>>> b34814c21f8604ff29f50f02572edd0904419129
+
 
