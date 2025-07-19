b2b165a0fdca1bd075f803697214ea63afa6e3de
Swetha-anbazhagan <swethaanbu2006@gmail.com>
2025-07-19 11:31:59 +0530

    merge conflicts

--- patients/urls.py
+++ patients/urls.py
@@ -1,20 +1,15 @@
 
 from django.urls import path
-<<<<<<< HEAD
+
 from . import views  
 
 urlpatterns = [
     
     path('patient_dashboard/',views.patient_dashboard,name='patient_dashboard'),
     path('doctor/',views.doctor,name='doctor'),
-    path('book/',views.book,name='book')
-=======
-from .import views
-
-
-urlpatterns = [
-    path('', views.patient_login, name='patient_login'),                # /patients/
-    
->>>>>>> b34814c21f8604ff29f50f02572edd0904419129
+    path('book/',views.book,name='book'),
+    path('', views.patient_login, name='patient_login'),     
 ]
+    
+
 
